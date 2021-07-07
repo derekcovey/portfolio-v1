fcb9ae5b3408dc785905d95e227dad142b15afa0
derekcovey <coveydrk@gmail.com>
2021-07-07 15:55:52 +0200

    feat: add img folder in mock to store images of the projects. add portfolio to the project list.

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,10 +4,10 @@ import { v4 as uuidv4 } from 'uuid'
 export const bigProjectCardData = [
     {
         id: uuidv4(),
-        desktopImg: 'https://via.placeholder.com/560x315?text=Desktop+View',
-        mobileImg: 'https://via.placeholder.com/135x240/A6A6A6/000000?text=Mobile+View',
-        name: 'Lorem ipsum dolor sit amet',
-        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut delectus a, nostrum rerum accusamus optio blanditiis aliquid rem inventore possimus doloribus saepe voluptas error praesentium ut. Fugiat harum maiores est. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestias a excepturi, recusandae nobis enim officiis, quidem saepe maiores molestiae magni fugit mollitia dolorum autem ratione doloribus modi quo nam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut delectus a, nostrum rerum accusamus optio blanditiis aliquid rem inventore possimus doloribus saepe voluptas error praesentium ut. Fugiat harum maiores est. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestias a excepturi, recusandae nobis enim officiis, quidem saepe maiores molestiae magni fugit mollitia dolorum autem ratione doloribus modi quo nam',
+        desktopImg: './img/portfolioDesktop.png',
+        mobileImg: './img/portfolioMobile.png',
+        name: 'Portfolio v1',
+        info: 'Here it is, my portfolio web-site where are you right now :). First of all I decided to do one for myself because in this way you can see what I can do, what I did, and what I am planning to do ^_^. I have used create-react-app as a react template for this project. Also scss as css preprocessor and BEM metodology to write understandable and logical className structure. You can check up the source code on github, link is included :).',
         githubLink: '',
         liveDemoLink: ''
     },
@@ -20,7 +20,6 @@ export const bigProjectCardData = [
         githubLink: '',
         liveDemoLink: ''
     },
-    
 ]
 
 // small project cards data
